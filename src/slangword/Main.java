c1fb8d5cb835f13dcaab8714da7714e41ea7820b
hadesduy0001 <hadesduy0001@gmail.com>
2021-04-20 15:23:26 +0700

    Xong chức năng thêm mới slang word

--- src/slangword/Main.java
+++ src/slangword/Main.java
@@ -5,7 +5,12 @@
  */
 package slangword;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Hashtable;
@@ -49,7 +54,7 @@ public class Main {
                     LichSuTimKiem(HtbHistory);
                 }
                 case 4 -> {
-                    ThemMotTuVung(htbKey);
+                    ThemMotTuVung(htbKey, htbMean);
                 }
                 case 5 -> {
                     SuaMotMotTuVung(htbKey);
@@ -121,6 +126,7 @@ public class Main {
                         System.out.println("Khóa `" + key + "` có nghĩa là `" + htbKey.get(key) + "`.");
                         HtbHistory.put(key, htbKey.get(key));
                     } else {
+                        HtbHistory.put(key, "Không tìm thấy nghĩa");
                         System.out.println("Hệ thông không tìm thấy nghĩa cho khóa là `" + key + "`.");
                     }
                 }
@@ -148,7 +154,8 @@ public class Main {
                 }
                 case 1 -> {
                     System.out.println("Mời bạn nhập nghĩa:");
-                    String mean = scanner.next();
+                    scanner.nextLine();
+                    String mean = scanner.nextLine();
                     mean = mean.trim().toLowerCase();
                     if (htbMean.get(mean) != null) {
                         String str = "Nghĩa của từ `" + mean + "` có các khóa là `";
@@ -161,6 +168,7 @@ public class Main {
                         HtbHistory.put(mean, str);
                         System.out.println(str);
                     } else {
+                        HtbHistory.put(mean, "Không tìm thấy khóa");
                         System.out.println("Hệ thông không tìm thấy khóa cho nghĩa là `" + mean + "`");
                     }
                 }
@@ -183,8 +191,167 @@ public class Main {
         }
     }
 
-    private static void ThemMotTuVung(Hashtable<String, String> htbKey) {
-        System.out.println("ok");
+    private static void ThemMotTuVung(Hashtable<String, String> htbKey, Hashtable<String, String> htbMean) throws IOException {
+        int functionID = 0;
+        do {
+            System.out.println("\n^^^ CHỨC NĂNG THÊM MỘT TỪ VỰNG(SLANG WORD) ^^^\n");
+            System.out.println("0. Thoát chức năng.");
+            System.out.println("1. Thêm từ vựng.");
+            System.out.println("MỜI BẠN CHỌN CHỨC NĂNG:");
+            Scanner scanner = new Scanner(System.in);
+            functionID = scanner.nextInt();
+            switch (functionID) {
+                case 0 -> {
+                }
+                case 1 -> {
+                    System.out.println("Mời bạn nhập khóa(key):");
+                    String key = scanner.next();
+                    System.out.println("Mời bạn nhập nghĩa(mean):");
+                    System.out.println("Ghi Chú: Nếu có nhiều nghĩa thì giữa các nghĩa nối nhau bằng dấu `|`.");
+                    scanner.nextLine();
+                    String mean = scanner.nextLine();
+                    mean = mean.trim().toLowerCase();
+                    if (htbKey.get(key.trim()) == null) {
+                        try {
+                            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("slang.txt", true)));
+                            out.println(key.trim() + "`" + mean.trim());
+                            out.flush();
+                            out.close();
+                            htbKey.put(key.trim(), mean.trim());
+                            String[] aMean = mean.trim().split("\\|");
+                            for (String aMean1 : aMean) {
+                                // chưa có nghĩa này trong htb mean
+                                if (htbMean.get(aMean1.trim().toLowerCase()) == null) {
+                                    htbMean.put(aMean1.trim().toLowerCase(), key);
+                                } else {
+                                    String temp = htbMean.get(aMean1.trim().toLowerCase());
+                                    temp = temp + "`" + key;
+                                    htbMean.put(aMean1.trim().toLowerCase(), temp);
+                                }
+                            }
+                            System.out.println("\n(^.^)Thêm từ vựng thành công(^.^).\n");
+                        } catch (IOException e) {
+                            System.out.println("\n(`.`)Thêm từ vựng thất bại(`.`).\n");
+                            println(e);
+                        }
+                    } else {
+                        int fId = 0;
+                        do {
+                            System.out.println("\n#### CƠ SỞ DỮ LIỆU ĐÃ TỒN TẠI SLANG WORD NÀY ####\n");
+                            System.out.println(key + " có nghĩa là ` " + htbKey.get(key.trim()) + " `.");
+                            System.out.println("Bạn có các sự lựa chọn:");
+                            System.out.println("0. Không thêm mới và quay lại.");
+                            System.out.println("1. Ghi đè dữ liệu(overwrite).");
+                            System.out.println("2.Thêm nghĩa mới cho khóa(key).");
+                            System.out.println("MỜI BẠN CHỌN CHỨC NĂNG:");
+                            fId = scanner.nextInt();
+                            switch (fId) {
+                                case 0 -> {
+                                }
+                                case 1 -> {
+                                    String tMean = htbKey.get(key.trim());
+                                    String[] aOldMean = tMean.split("\\|");
+                                    String[] aNewMean = mean.trim().split("\\|");
+                                    GhiDeDuLieuMotDong(key.trim() + "`" + htbKey.get(key.trim()), key.trim() + "`" + mean);
+                                    // cập nhật cơ sở dữ liêu thành công
+                                    htbKey.put(key.trim(), mean.trim());
+                                    // xóa các nghĩa cũ có key này
+                                    for (String oldMean : aOldMean) {
+                                        if (htbMean.get(oldMean.trim()) != null) {
+                                            String temp = htbMean.get(oldMean.trim());
+                                            temp = temp.replace(key.trim(), "");
+                                            temp = temp.replace("``", "`");
+                                            if (temp.length() < 2) {
+                                                temp = "";
+                                                htbMean.remove(oldMean.trim());
+                                            } else {
+                                                htbMean.put(oldMean.trim(), temp);
+                                            }
+                                        }
+                                    }
+                                    // cập nhật nghĩa mới
+                                    for (String newMean : aNewMean) {
+                                        if (htbMean.get(newMean.trim()) != null) {
+                                            String temp = htbMean.get(newMean.trim());
+                                            temp = temp + "`" + key.trim();
+                                            htbMean.put(newMean.trim(), temp);
+                                        } else {
+                                            htbMean.put(newMean.trim(), key.trim());
+                                        }
+                                    }
+                                    System.out.println("\n(^.^)Thêm từ vựng thành công(^.^).\n");
+                                }
+                                case 2 -> {
+                                    String tMean = htbKey.get(key.trim());
+                                    String[] aOldMean = tMean.split("\\|");
+                                    String[] aNewMean = mean.trim().split("\\|");
+                                    ArrayList<String> trueNewMean = new ArrayList<>();
+                                    boolean check = false;
+                                    // lây ra các nghĩa mới còn trùng thì thôi
+                                    for (int i = 0; i < aNewMean.length; i++) {
+                                        check = false;
+                                        for (int j = 0; j < aOldMean.length; j++) {
+                                            if (aNewMean[i].trim() == aOldMean[j].trim()) {
+                                                check = true;
+                                            }
+                                        }
+                                        if (check == false) {
+                                            trueNewMean.add(aNewMean[i]);
+                                        }
+                                    }
+                                    String str = "";
+                                    if (trueNewMean.size() > 0) {
+                                        for (int i = 0; i < trueNewMean.size() - 1; i++) {
+                                            str = str + trueNewMean.get(i) + "|";
+                                        }
+                                        str = str + trueNewMean.get(trueNewMean.size() - 1);
+                                        if (str != "") {
+                                            GhiDeDuLieuMotDong(key.trim() + "`" + htbKey.get(key.trim()), key.trim() + "`" + htbKey.get(key.trim()) + "|" + str);
+                                        }
+                                        for (int i = 0; i < trueNewMean.size(); i++) {
+                                            if (htbMean.get(trueNewMean.get(i).trim()) != null) {
+                                                String temp = htbMean.get(trueNewMean.get(i).trim());
+                                                temp = temp + "`" + key.trim();
+                                                htbMean.put(htbMean.get(trueNewMean.get(i).trim()), temp);
+                                            } else {
+                                                htbMean.put(trueNewMean.get(i).trim(), key.trim());
+                                            }
+                                        }
+                                    }
+                                    htbKey.put(key.trim(), htbKey.get(key.trim()) + "|" + str);
+                                    System.out.println("\n(^.^)Thêm từ vựng thành công(^.^).\n");
+                                }
+                                default -> {
+                                    System.out.println("\n=============================THÔNG BÁO=============================");
+                                    System.out.println("Bạn nhập mã chức năng không đúng. Vui lòng nhập lại.");
+                                    System.out.println("\n=============================THÔNG BÁO=============================");
+                                }
+                            }
+                        } while (fId > 2 && fId < 0);
+                    }
+                }
+                default -> {
+                    System.out.println("\n=============================THÔNG BÁO=============================");
+                    System.out.println("Bạn nhập mã chức năng không đúng. Vui lòng nhập lại.");
+                    System.out.println("\n=============================THÔNG BÁO=============================");
+                }
+            }
+        } while (functionID != 0);
+    }
+
+    private static void GhiDeDuLieuMotDong(String oldLine, String newLine) throws FileNotFoundException, IOException {
+        String filePath = "slang.txt";
+        Scanner sc = new Scanner(new File(filePath));
+        StringBuffer buffer = new StringBuffer();
+        while (sc.hasNextLine()) {
+            buffer.append(sc.nextLine() + System.lineSeparator());
+        }
+        String fileContents = buffer.toString();
+        sc.close();
+        fileContents = fileContents.replace(oldLine, newLine);
+        FileWriter writer = new FileWriter(filePath);
+        writer.append(fileContents);
+        writer.flush();
     }
 
     private static void SuaMotMotTuVung(Hashtable<String, String> htbKey) {
@@ -209,6 +376,10 @@ public class Main {
 
     private static void NghiaTimKyHieu(Hashtable<String, String> htbKey) {
         System.out.println("ok");
+    }
+
+    private static void println(IOException e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
